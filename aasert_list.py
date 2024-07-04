1a2147aa6db14437fca7ca5b5328ed06acb407c9
annepuarun <161306263+annepuarun@users.noreply.github.com>
2024-07-04 22:19:06 +0530

    Add files via upload

---
+++ aasert_list.py
@@ -0,0 +1,13 @@
+'''def avg(marks):
+    assert len(marks) !=0,"The List is empty."
+    return sum(marks)/len(marks)
+
+marks =[67,59,86,75,92]
+print("The Average of Marks1:",avg(marks))
+print("The Average of Marks:",avg(marks))
+'''
+list123=[1,2,33,7,9,0]
+x=6
+assert x not in list123," x is not in list"
+
+assert x  in list123,"x in list"
